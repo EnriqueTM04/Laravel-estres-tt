a74ff1927af3b83bd623965e8daa098d37a7e1e0
EnriqueTM04 <enriquetabo03@gmail.com>
2025-10-31 15:06:11 -0600

    Revocación de acceso en logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,6 +33,11 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request) {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
 
+        return [
+            'user' => null
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController:: class, 'logout']);
+});
 
 // Autenticacion
 Route::post('login', [AuthController::class, 'login']);
